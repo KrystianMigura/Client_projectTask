c0e13ae41c06519250cd76b55e6ba3641cf4b036
KrystianMigura <Krystian.migura@gmail.com>
2019-12-04 19:58:56 +0100

    simple client
    simple client

---
+++ Client/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using NetworkCommsDotNet;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting.....");
+
+            tcpclnt.Connect("127.0.0.1", 10000);
+            // use the ipaddress as in the server program
+
+            Console.WriteLine("Connected");
+            Console.Write("Enter the string to be transmitted : ");
+
+            String str = "to jest jakis moj tam text";
+            
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(str);
+            Console.WriteLine("Transmitting.....");
+
+
+
+
+
+            Stream stm = tcpclnt.GetStream();
+
+            stm.Write(ba, 0, ba.Length);
+
+            byte[] bb = new byte[256];
+            int k = stm.Read(bb, 0, 256);
+            string test="" ;
+            for (int i = 0; i < k; i++)
+                test += Convert.ToChar(bb[i]);
+
+            Console.WriteLine(test);// automatyczna odpowiedź z servera !! jupi
+
+
+
+        }
+    }
+}
