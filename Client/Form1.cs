1256997484192a55e0e50521f81b1eb666845562
KrystianMigura <Krystian.migura@gmail.com>
2019-12-05 19:48:45 +0100

    new button for send message
    new button for sernd message

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -16,14 +16,23 @@ namespace Client
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
-        }
 
-        private void button1_Click(object sender, EventArgs e)
+        }
+        private TcpClient tcpclnt { get;set;} 
+        public void Connect()
         {
             TcpClient tcpclnt = new TcpClient();
+            tcpclnt.Connect("127.0.0.1", 10000);
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.tcpclnt = new TcpClient();
             Console.WriteLine("Connecting.....");
 
             tcpclnt.Connect("127.0.0.1", 10000);
@@ -56,6 +65,33 @@ namespace Client
             Console.WriteLine(test);// automatyczna odpowiedź z servera !! jupi
 
 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String str = "to jest jakis moj tam text";
+
+
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] ba = asen.GetBytes(str);
+            Console.WriteLine("Transmitting.....");
+
+
+
+           
+
+            Stream stm = tcpclnt.GetStream();
+
+            stm.Write(ba, 0, ba.Length);
+
+            byte[] bb = new byte[256];
+            int k = stm.Read(bb, 0, 256);
+            string test = "";
+            for (int i = 0; i < k; i++)
+                test += Convert.ToChar(bb[i]);
+
+            Console.WriteLine(test);// automatyczna odpowiedź z servera !! jupi
 
         }
     }
