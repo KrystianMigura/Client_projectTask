e63d891d7fb76b0f0f2ba7accdd2ad41ae6042b3
KrystianMigura <Krystian.migura@gmail.com>
2019-12-22 11:23:05 +0100

    corrected value

---
+++ Client/Users/RegisterNewUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Users
+{
+   class RegisterNewUser
+    {
+        public RegisterNewUser() { }
+
+        protected string userName { get; set; }
+        protected string pass { get; set; }
+
+        public void catchData(String userName, string password, Client.Options.ConnectToServer clnt)
+        {
+            this.userName = userName;
+            this.pass = password;
+            clnt.sendMessage("#102~ " + this.userName + "/" + this.pass);
+        }
+
+    }
+}
